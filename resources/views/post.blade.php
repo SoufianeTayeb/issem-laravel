e5483c644d9571513a523bbc2484cec5ff815025
SoufianeTayeb <soufiane.tayeb2000@gmail.com>
2023-10-24 14:48:06 +0100

    commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@yield('layout')
+@section('titre')
+   Page post
+@endsection
+@section('content')
+
+    <li>{{$titre_post}}</li>
+    
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,35 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/test/{nombre?}',function($nombre=null){
-    return view('test',['nombre'=>$nombre]);
-});
+// Route::get('/test',function(){
+//     $etudiant = [
+        
+//         1=>"soufiane",
+//         2=>"hassan",
+//         3=>"fatima",
+//     ];
+//     return view('test',['etudiant'=>$etudiant]);
+// });
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// // Route::get('/home/{id}/{naem}', function ($id, $name) {
+// //     $data = ["id" => $id, "name" => $name];
+// //     return view('home',['data'=> $data]);})->where('id','[0-9]+');
+// Route ::get('/home/{id}/{name}',[user_controller::class,'index']);
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-// Route::get('/home/{id}/{naem}', function ($id, $name) {
-//     $data = ["id" => $id, "name" => $name];
-//     return view('home',['data'=> $data]);})->where('id','[0-9]+');
-Route ::get('/home/{id}/{name}',[user_controller::class,'index']);
+Route::get('/post/{id_post}', function ($id_post) {
+    $posts=[
+        1=>'Apprendre Laravel',
+        2=>'Apprendre Python',
+        3=>'Apprendre Javascript',
+        4=>'Apprendre Java',
+        5=>'Apprendre C',
+        6=>'Apprendre C#',
+        7=>'Apprendre React.js',
+        8=>'Apprendre Node.js'
+    ];
+    return  view('post',['titre_post' => $posts[$id_post]]);
+});
